37d79152e6f9550a1acbdd9a8e4ca3b4521ae9dc
Tanev <66444552+TanevAZ@users.noreply.github.com>
2024-04-24 13:02:15 +0200

    Ajout pour form php

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, ['label' => 'Titre'])
+            ->add('texte1', TextareaType::class, ['label' => 'Texte 1', 'required' => false])
+            ->add('texte2', TextareaType::class, ['label' => 'Texte 2', 'required' => false])
+            ->add('url', UrlType::class, ['label' => 'URL', 'required' => false])
+            ->add('save', SubmitType::class, ['label' => 'Créer Article']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
